b0f58f85953361f292f5ac56a2d280b3d810804a
Alex Forbes-Reed <alexreed@outlook.com>
2015-05-19 01:20:41 +0100

    xbox live auth test

--- app.js
+++ app.js
@@ -11,45 +11,44 @@ router.route('/').get(function(req, res, next) {
 	res.json({ "urm...": "you're in the wrong part of the right place." });
 });
 
-router.route('/api/v1/halo4').post(function(req, res, next) {
-	if (req.body == undefined) {
-		res.json({ result: null, error: { error_description: "no_account_information" } });
-		return;
-	}
-
-	var microsoftAccount = null;
-	var microsoftAccountPassword = null;
-
-	if (req.body["MicrosoftAccount"] != undefined)
-		microsoftAccount = req.body["MicrosoftAccount"];
-	if (req.body["MicrosoftAccountPassword"] != undefined)
-		microsoftAccountPassword = req.body["MicrosoftAccountPassword"];
-
-	if (microsoftAccount == null || microsoftAccountPassword == null) {
-		res.json({ Result: null, Error: { Description: "no_account_information" } });
-		return;
-	}
-
-	var browser = new Browser();
-	Browser.userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.104 Safari/537.36";
-	browser.visit("https://app.halowaypoint.com/oauth/spartanToken", function() {
-		browser.
-			fill("input[type=email]", microsoftAccount).
-			fill("input[type=password]", microsoftAccountPassword).
-			pressButton("Sign in", function() {
-				if (browser.text('body').indexOf('SpartanToken') != -1) {
-					var data = browser.text('body');
-					res.json({ Result: JSON.parse(data), Error: null });
-					browser.close();
-				} else {
-					res.json({ Result: null, Error: { Description: "unable_to_authentication_with_halo_waypoint", details: browser.text('body') } });
-					browser.close();
-					return;
-				}
-			});
-	});
-});
-
+//router.route('/api/v1/halo4').post(function(req, res, next) {
+//	if (req.body == undefined) {
+//		res.json({ result: null, error: { error_description: "no_account_information" } });
+//		return;
+//	}
+//
+//	var microsoftAccount = null;
+//	var microsoftAccountPassword = null;
+//
+//	if (req.body["MicrosoftAccount"] != undefined)
+//		microsoftAccount = req.body["MicrosoftAccount"];
+//	if (req.body["MicrosoftAccountPassword"] != undefined)
+//		microsoftAccountPassword = req.body["MicrosoftAccountPassword"];
+//
+//	if (microsoftAccount == null || microsoftAccountPassword == null) {
+//		res.json({ Result: null, Error: { Description: "no_account_information" } });
+//		return;
+//	}
+//
+//	var browser = new Browser();
+//	Browser.userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.104 Safari/537.36";
+//	browser.visit("https://app.halowaypoint.com/oauth/spartanToken", function() {
+//		browser.
+//			fill("input[type=email]", microsoftAccount).
+//			fill("input[type=password]", microsoftAccountPassword).
+//			pressButton("Sign in", function() {
+//				if (browser.text('body').indexOf('SpartanToken') != -1) {
+//					var data = browser.text('body');
+//					res.json({ Result: JSON.parse(data), Error: null });
+//					browser.close();
+//				} else {
+//					res.json({ Result: null, Error: { Description: "unable_to_authentication_with_halo_waypoint", details: browser.text('body') } });
+//					browser.close();
+//					return;
+//				}
+//			});
+//	});
+//});
 
 router.route('/api/v1/xboxlive').post(function(req, res, next) {
 	if (req.body == undefined) {
